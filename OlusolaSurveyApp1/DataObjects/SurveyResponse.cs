2dbe41323d997220e68300b112612f76204768e6
solaadio <olusola.adio@logion.co.uk>
2017-05-10 00:07:05 +0100

    added controller

---
+++ OlusolaSurveyApp1/DataObjects/SurveyResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Azure.Mobile.Server;
+using Newtonsoft.Json;
+
+namespace OlusolaSurveyApp1.DataObjects
+{
+    [Table("responses")]
+    public class SurveyResponse : EntityData
+    {
+        [JsonProperty("questionId")]
+        public string QuestionId { get; set; }
+
+        public string Name { get; set; }
+        [JsonProperty("answer")]
+        public int AnswerIndex { get; set; }
+    }
+}

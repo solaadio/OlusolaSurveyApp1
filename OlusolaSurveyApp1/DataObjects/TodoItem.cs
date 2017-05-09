b94f0013862f953f2055f57a724ee8a0363b41f3
solaadio <olusola.adio@logion.co.uk>
2017-05-09 23:15:43 +0100

    Add project files.

---
+++ OlusolaSurveyApp1/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace OlusolaSurveyApp1.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
